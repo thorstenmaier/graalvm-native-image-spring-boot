1e62a552288ee7bea569f62c3aa23f8846ff3dde
Thorsten Maier <Thorsten.Maier@oio.de>
2020-04-23 16:58:19 +0200

    Added output of used memory

--- plain-java-with-reflection/src/main/java/de/oio/PlainJavaWithReflection.java
+++ plain-java-with-reflection/src/main/java/de/oio/PlainJavaWithReflection.java
@@ -10,6 +10,19 @@ public class PlainJavaWithReflection {
 		Object instance = class1.newInstance();
 		Method method = class1.getMethod("doIt");
 		method.invoke(instance);
+		
+		System.out.println("Max memory:   " + formatSize(Runtime.getRuntime().maxMemory()));
+		System.out.println("Total memory: " + formatSize(Runtime.getRuntime().totalMemory()));
+		System.out.println("Free memory:  " + formatSize(Runtime.getRuntime().freeMemory()));
+		System.out.println("Used memory:  " + formatSize(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
 	}
+	
+    public static String formatSize(long v) {
+        if (v < 1024) return v + " B";
+        int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
+        return String.format("%.1f %sB", (double)v / (1L << (z*10)), " KMGTPE".charAt(z));
+    	
+//    	return String.format("%12.12s", v);
+    }
 
 }
