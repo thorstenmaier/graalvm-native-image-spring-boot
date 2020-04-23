31fe594d6dce824c90f3698daff060aa8bafde34
Nurmukhammad Begiboev <nurmukhammad.begiboev@oio.de>
2020-04-23 14:34:45 +0200

    main method imporved....

--- demoGVM/src/main/java/com/trivadis/DemoGvmApplication.java
+++ demoGVM/src/main/java/com/trivadis/DemoGvmApplication.java
@@ -2,12 +2,19 @@ package com.trivadis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoGvmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoGvmApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(DemoGvmApplication.class, args);
+		RestTemplate  restTemplate = new RestTemplate();
+		String obj = restTemplate.getForObject("http://localhost:8089/", String.class);
+		System.out.println(" ===  http://localhost:8089/ : " + obj);
+		ctx.close();
+	
 	}
 
 }
