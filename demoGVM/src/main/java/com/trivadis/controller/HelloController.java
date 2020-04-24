b05636061f97cee5c0326a76408cff9ab84096e0
Thorsten Maier <Thorsten.Maier@oio.de>
2020-04-24 14:32:56 +0200

    Application Config for graalvm feature path

--- demoGVM/src/main/java/com/trivadis/controller/HelloController.java
+++ demoGVM/src/main/java/com/trivadis/controller/HelloController.java
@@ -3,7 +3,7 @@ package com.trivadis.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class HelloController {
 
 	@GetMapping("/")
