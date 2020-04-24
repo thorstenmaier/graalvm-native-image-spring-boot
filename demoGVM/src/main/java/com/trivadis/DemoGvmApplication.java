12e7647b9b9bf9e28d9ed75e0986650b3f519297
Thorsten Maier <Thorsten.Maier@oio.de>
2020-04-24 14:08:42 +0200

    Ausgabe der Speicherauslastung in Spring Boot Web-App eingebaut

--- demoGVM/src/main/java/com/trivadis/DemoGvmApplication.java
+++ demoGVM/src/main/java/com/trivadis/DemoGvmApplication.java
@@ -10,12 +10,25 @@ public class DemoGvmApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(DemoGvmApplication.class, args);
-		RestTemplate  restTemplate = new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
 		String obj = restTemplate.getForObject("http://localhost:8089/", String.class);
-		
+
 		System.out.println(" ===  http://localhost:8089/ : " + obj);
+
+		System.out.println("Max memory:   " + formatSize(Runtime.getRuntime().maxMemory()));
+		System.out.println("Total memory: " + formatSize(Runtime.getRuntime().totalMemory()));
+		System.out.println("Free memory:  " + formatSize(Runtime.getRuntime().freeMemory()));
+		System.out.println(
+				"Used memory:  " + formatSize(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+
 		ctx.close();
-	
+	}
+
+	public static String formatSize(long v) {
+		if (v < 1024)
+			return v + " B";
+		int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
+		return String.format("%.1f %sB", (double) v / (1L << (z * 10)), " KMGTPE".charAt(z));
 	}
 
 }
