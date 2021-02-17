061fdfacd79fc2076d7cda3a3c08b1427d78adae
Thorsten Maier <Thorsten.Maier@oio.de>
2021-02-17 09:03:16 +0100

    Remove deprecrated method call

--- plain-java-with-reflection/src/main/java/de/oio/PlainJavaWithReflection.java
+++ plain-java-with-reflection/src/main/java/de/oio/PlainJavaWithReflection.java
@@ -4,10 +4,9 @@ import java.lang.reflect.Method;
 
 public class PlainJavaWithReflection {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws Exception {
 		Class<?> class1 = Class.forName("de.oio.MyComponent");
-		Object instance = class1.newInstance();
+		Object instance = class1.getDeclaredConstructor().newInstance();
 		Method method = class1.getMethod("doIt");
 		method.invoke(instance);
 		
